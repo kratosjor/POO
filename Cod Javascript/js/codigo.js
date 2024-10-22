24820297e1d2de5894d2e04fca1dd58d8eed19b7
Jordan <jordan.espinoza.c@gmail.com>
2024-10-21 21:41:51 -0300

    Sistema con JS e IA

---
+++ Cod Javascript/js/codigo.js
@@ -0,0 +1,31 @@
+//alert("Hola mundo desde JS");
+/*
+let numero = 12;                    //numerico
+let numero2 = 2.635;                //numerico
+let direccion = "San martin 1320"   //string
+let estado = true;                  //boleano
+
+let nombre = prompt("Ingrese su nombre: ")
+
+if (nombre == "root"){
+    alert("Acceso Correcto!");
+}else{
+    alert("Acceso Denegado");
+}
+
+
+// Solicitar el nombre al usuario
+let nombre = prompt("Por favor, ingresa tu nombre:");
+
+// Mostrar mensaje de bienvenida
+alert("¡Bienvenido(a), " + nombre + "!");
+*/
+
+// Solicitar el nombre al usuario
+let nombre = prompt("Por favor, ingresa tu nombre:");
+
+// Solicitar el apellido al usuario
+let apellido = prompt("Por favor, ingresa tu apellido:");
+
+// Mostrar el nombre completo en una ventana emergente
+alert("Hola " + nombre + " " + apellido + ", ¡bienvenido(a)!");
---
+++ clase1.py
@@ -0,0 +1,17 @@
+"""
+git config --gobal user.name ""
+git config --gobal user.email ""
+
+git init (iniciala)
+
+git status (ver el estado del repositorio)
+git add . (pasamos los archivos al stating area)
+git commit (i para insertar texto, ESC para salir, :wq para guardar enter para volver)
+git commit -m "Descripcion" , esta es otra forma de hacer una descripcion
+
+"""
+"""
+CLASE 2
+
+
+"""
